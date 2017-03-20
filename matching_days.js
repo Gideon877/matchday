61058d6ec6eb5a3fe3e5eeb8519133b6f3ef66d4
gideon877 <tblar84@gmail.com>
2017-03-20 12:26:34 +0200

    first commit

--- matching_days.js
+++ matching_days.js
@@ -8,30 +8,41 @@ var days = document.querySelectorAll('.days');
 
 function myFunction() {
 
-    for (var i = 0; i < days.length; i++) { // loop through days
-        var newDate1 = new Date(date1.value);// convert input date to js date
-        var whichDag1 = newDate1.getDay(); // get the value of the day
 
-        var newDate2 = new Date(date2.value);
-        var whichDag2 = newDate2.getDay();
+    for (var i = 0; i < days.length; i++) { // loop through days
 
         days[i].classList.remove("mydate1");
         days[i].classList.remove("mydate2");
         days[i].classList.remove("mystyle");
 
-        if(whichDag1 === whichDag2){
+        if(date1.value !== ""){
+            var newDate1 = new Date(date1.value);
+            var whichDag1 = newDate1.getDay();
+        } else {
+            var whichDag1 = "";
+        }
+        if(date2.value !== ""){
+            var newDate2 = new Date(date2.value);
+            var whichDag2 = newDate2.getDay();
+        } else {
+            var whichDag2 = "";
+        }
+
+
+        if(whichDag1 === whichDag2 && whichDag1 !== "" && whichDag2 !== ""){
             days[whichDag1].classList.add("mystyle");   //give the day green colour
         }
-        else if( whichDag2 == NaN){
-            days[whichDag1].classList.add('mydate1');
-            //days[whichDag2].classList.add('mydate2');
-
-        } else if (whichDag1 == NaN) {
-            days[whichDag2].classList.add('mydate2');
-
-        } else {
+        else if( whichDag1 !== "" && whichDag2 !== "" && whichDag1 !== whichDag2){
             days[whichDag1].classList.add('mydate1');
             days[whichDag2].classList.add('mydate2');
-        }
+
+        }else if (whichDag1 !== "" && whichDag1 !== whichDag2 && whichDag2 === "") {
+            days[whichDag1].classList.add('mydate1');
+
+
+        } else if (whichDag2 !== "" && whichDag1 !== whichDag2 && whichDag1 === ""){
+            days[whichDag2].classList.add('mydate2');
+        //     days[whichDag1].classList.add('mydate1');
+         }
     }
 }
